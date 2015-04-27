6032b7a9ce7d8df811af041b0405a825ce019089
Guest User <student@epicodus.com>
2015-04-27 13:41:20 -0700

    It creates logic for store,fetch and has_key methods

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -3,27 +3,20 @@ require ('pry')
 
 class MyHash
   define_method(:initialize) do
-  @key_array = Array.new()
-  @value_array = Array.new()
-
-
+    @key_array = Array.new()
+    @value_array = Array.new()
   end
-
   define_method(:store) do |key, value|
     @key = key
     @value = value
-
     @key_array.push(key)
     @value_array.push(value)
-
-
   end
-
-  define_method(:fetch) do |key|
-    key_index = @key_array.index(key)
-    array_index = @value_array[key_index]
-
-
-
- end
-end
+    define_method(:fetch) do |key|
+      key_index = @key_array.index(key)
+      array_index = @value_array[key_index]
+  end
+    define_method(:has_key?) do |key|
+      @key_array.include?(key)
+      end
+    end
