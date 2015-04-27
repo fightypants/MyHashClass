84e15f61e44c6ed1a0b67c8b150ea1ad43ea9c97
Guest User <student@epicodus.com>
2015-04-27 13:59:00 -0700

    Passes test for fetch and has_key methods

--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -9,4 +9,19 @@ describe('MyHash') do
       expect(test_hash.fetch('kitten')).to(eq('cute'))
     end
   end
+    describe('#fetch') do
+      it('retrieves a stored value by its key') do
+        test_hash = MyHash.new()
+        test_hash.store('kitten', 'cute')
+        expect(test_hash.fetch('kitten')).to(eq('cute'))
+      end
+    end
+
+    describe('#has_key?') do
+      it('retrieves a stored value by its key') do
+        test_hash = MyHash.new()
+        test_hash.store('kitten', 'cute')
+        expect(test_hash.has_key?('kitten')).to(eq(true))
+      end
+    end
 end
